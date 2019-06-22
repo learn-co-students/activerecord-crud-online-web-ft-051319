e614baf59099bed7944b26fd60a435e6774c4fc1
M Banks <montensia1@outlook.com>
2019-06-22 04:14:19 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movies.create
   movie.title = "This is a title."
   __
 end
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(args = __)
   # release_date == 1990
   
   Movie.create do |m|
-    __
+    m.string :title
   end
 end
 
